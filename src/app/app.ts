9bbfc9263efe5c247aa108aaea291ca38f96ccbe
Matheus Amaral da Costa <161625293+MatheusADC@users.noreply.github.com>
2025-06-10 16:47:59 -0300

    fix: Path das importações

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,11 +6,12 @@ import { PrimaryButton } from "./_components/primary-button/primary-button";
 import { SecondaryButton } from "./_components/secondary-button/secondary-button";
 import { ItemCertificado } from "./_components/item-certificado/item-certificado";
 import { BaseUi } from "./_components/base-ui/base-ui";
-import { Certificados } from "../pages/certificados/certificados";
+import { CertificadoForm } from "./pages/certificado-form/certificado-form";
+import { Certificados } from './pages/certificados/certificados';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, CommonModule, PrimaryButton, SecondaryButton, ItemCertificado, BaseUi, Certificados],
+  imports: [RouterOutlet, Navbar, CommonModule, PrimaryButton, SecondaryButton, ItemCertificado, BaseUi, Certificados, CertificadoForm],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
