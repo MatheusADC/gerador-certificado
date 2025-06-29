01e14c538fd1c434543e1800427227b40a76bbd5
Matheus Amaral da Costa <161625293+MatheusADC@users.noreply.github.com>
2025-06-29 16:51:15 -0300

    add: Implementação da interface Certificado & Criação da função de submit

--- src/app/pages/certificado-form/certificado-form.ts
+++ src/app/pages/certificado-form/certificado-form.ts
@@ -3,33 +3,44 @@ import { SecondaryButton } from "../../_components/secondary-button/secondary-bu
 import { PrimaryButton } from "../../_components/primary-button/primary-button";
 import { FormsModule, NgModel } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { Certificado } from '../../interfaces/certificado';
 
 @Component({
   selector: 'app-certificado-form',
+  // Para poder usar o ngModel tem que importar o FormsModule. O ngModel permite receber as variáveis em tempo real (two way binding). Pesquisa també sobre one way binding.
+  // Para aplicar o estilo dinamicamente com ngstyle tem que importar o commom module.
   imports: [SecondaryButton, PrimaryButton, FormsModule, CommonModule],
   templateUrl: './certificado-form.html',
   styleUrl: './certificado-form.css'
 })
 export class CertificadoForm {
-  nome: string = '';
+
+  certificado: Certificado = {
+    atividades: [],
+    nome: ''
+  };
   atividade: string = '';
-  atividades: string[] = [];
 
   campoInvalido(control: NgModel) {
     return control.invalid && control.touched
   }
 
   formValido() {
-    return this.atividades.length > 0 && this.nome.length > 0;
+    return this.certificado.atividades.length > 0 && this.certificado.nome.length > 0;
   }
 
   adicionarAtividade() {
-    this.atividades.push(this.atividade);
+    this.certificado.atividades.push(this.atividade);
     this.atividade = '';
   }
 
   excluirAtividade(index: number) {
-    this.atividades.splice(index, 1);
+    this.certificado.atividades.splice(index, 1);
   }
-}
+
+  submit() {
+    if(!this.formValido()) {
+      return;
+    }
+  }
 }
