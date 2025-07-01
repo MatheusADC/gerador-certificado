a25494084c9ec0f78d7a93802998e531dbdb3bb2
Matheus Amaral da Costa <161625293+MatheusADC@users.noreply.github.com>
2025-07-01 11:51:14 -0300

    add: Obtendo o certificado com localStorage

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,21 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./_components/navbar/navbar";
 import { CommonModule } from '@angular/common';
-import { PrimaryButton } from "./_components/primary-button/primary-button";
-import { SecondaryButton } from "./_components/secondary-button/secondary-button";
-import { ItemCertificado } from "./_components/item-certificado/item-certificado";
 import { BaseUi } from "./_components/base-ui/base-ui";
-import { CertificadoForm } from "./pages/certificado-form/certificado-form";
-import { Certificados } from './pages/certificados/certificados';
+import { Certificado as CertificadoService } from './_services/certificado';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, CommonModule, PrimaryButton, SecondaryButton, ItemCertificado, BaseUi, Certificados, CertificadoForm],
+  imports: [RouterOutlet, Navbar, CommonModule, BaseUi,  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected title = 'gerador-certificado';
   exibeNavbar: boolean = false;
+
+  constructor(private certificadoService: CertificadoService) {}
+
+  ngOnInit(): void {
+      const certificados = localStorage.getItem('certificados');
+      this.certificadoService.certificados = certificados ? JSON.parse(certificados) : [];
+  }
 }
