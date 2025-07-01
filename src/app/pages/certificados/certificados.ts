6f466787b144106eed0ddd2f83c536cc77da3dfc
Matheus Amaral da Costa <161625293+MatheusADC@users.noreply.github.com>
2025-07-01 11:24:16 -0300

    add: Implementações do serviço & ngOnInit()

--- src/app/pages/certificados/certificados.ts
+++ src/app/pages/certificados/certificados.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ItemCertificado } from "../../_components/item-certificado/item-certificado";
 import { SecondaryButton } from "../../_components/secondary-button/secondary-button";
 import { RouterLink } from '@angular/router';
+import { Certificado as certificadoService} from '../../_services/certificado';
+import { Certificado } from '../certificado/certificado';
 
 
 @Component({
@@ -10,6 +12,13 @@ import { RouterLink } from '@angular/router';
   templateUrl: './certificados.html',
   styleUrl: './certificados.css'
 })
-export class Certificados {
+export class Certificados implements OnInit{
 
+  certificados: Certificado[] = [];
+
+  constructor(private certificadoService: certificadoService) {}
+
+  ngOnInit(): void {
+      this.certificados = this.certificadoService.certificados;
+  }
 }
